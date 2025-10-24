3413a260a9e137187e2c6c1ff837ee2a514555f6
Branislav Juhás <juhas.branislaw@gmail.com>
2025-10-24 21:05:18 +0200

    feat: initialize Gasbag project with basic structure and configuration files

---
+++ Gasbag/Program.cs
@@ -0,0 +1,11 @@
+﻿using SadConsole.Configuration;
+
+Settings.WindowTitle = "My SadConsole Game";
+
+Builder
+    .GetBuilder()
+    .SetWindowSizeInCells(80, 25)
+    .SetStartingScreen<Gasbag.Scenes.Menu>()
+    .IsStartingScreenFocused(true)
+    .ConfigureFonts(true)
+    .Run();
